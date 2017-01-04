ef4901122c292b22bfb64864555d4d168abf2ced
zahid mahir <zahid@zmhr.me>
2017-01-03 23:22:56 -0500

    Make simple app do display a fact

--- client/app.js
+++ client/app.js
@@ -1,1 +1,21 @@
-console.log("hello");
+$(document).ready(function() {
+
+    $(window).resize(function(){
+        let left = ($(window).width() - $('.text').outerWidth()) /2 ;
+        let top = ($(window).height() - $('.text').outerHeight())/2;
+        console.log(left, top)
+        $('.text').css({
+            position:'absolute',
+            left: left,
+            top: top
+        });
+
+    });
+
+    $.getJSON('http://localhost:8080/api/fact', function(data) {
+        $(".text").text(data.text);
+        $("body").css("background-image", "url(" + data.image + ".jpg)");
+        $(window).resize();
+    });
+    
+})
